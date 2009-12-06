6fb1dcedcff27186cff7e933fba12f87cb442b14
Wojciech Daniel Meyer <wojciech.meyer@gmail.com>
2009-12-06 02:31:32 +0000

    actually it works\! demo size ~ 1800 bytes

--- image4k/image.ml
+++ image4k/image.ml
@@ -607,7 +607,7 @@ module FourkImage = struct
       Words.words (word_sec,name_sec)
 
     let stripped_sections =  ["interpret";"name";"dsptch";"semantic";] 
-    let stripped_sections =  []
+(*    let stripped_sections =  [] *)
 (* 92 *)
   let strip image =
   (*  let secs = List.fold_left (fun acc i -> List.filter (fun x -> not (i = x.Image.name)) acc) image.Image.sections removed_sections in *)
